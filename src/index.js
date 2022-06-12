a6673f329c19d89b1250cb32afaee0854853f013
MarceloArraes <marcelo.arraes@gmail.com>
2022-06-12 01:57:36 +0000

    feat: add a env file with mongoose info to connect to mongooseDB

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 const express = require('express');
+require('dotenv').config({path: './.env'});
+const mongoose = require('mongoose');
+
 
 const app = express();
 
+const mongoUri = process.env.MONGOURI
+
+mongoose.connect(mongoUri, {
+useNewUrlParser: true,
+});
+mongoose.connection.on('connected', () => {
+console.log('Connected to mongo instance');
+})
+mongoose.connection.on('error', (err) => {
+console.error('Error connecting to mongo', err);
+}
+) 
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
 });

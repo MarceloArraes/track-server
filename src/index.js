7194989f8ce1474b472a6e076de72e8b2066464f
MarceloArraes <marcelo.arraes@gmail.com>
2022-06-13 11:13:53 +0000

    feat: created tracks in mongoose model and add one example.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 require('./models/User');
 require('dotenv').config({path: './.env'});
+require('./models/Track');
 const mongoose = require('mongoose');
 const express = require('express');
 const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
 const bodyParser = require('body-parser');
 const requireAuth = require('./middlewares/requireAuth');
 
@@ -11,6 +13,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri = process.env.MONGOURI
 
---
+++ src/routes/trackRoutes.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireAuth = require('../middlewares/requireAuth');
+
+const Track = mongoose.model('Track');
+
+const router = express.Router();
+
+router.use(requireAuth);
+
+router.get('/tracks', async (req, res) =>{
+  const tracks = await Track.findOne({userId: req.user._id});
+  console.log('req.user', req.user);
+  console.log('tracks', tracks);
+  res.send(tracks);
+})
+
+router.post('/tracks', async (req, res)=>{
+  const {name, locations} = req.body;
+
+  if(!name || !locations) return res.status(422).send({error: 'You must provide a name and locations'});
+  try{
+    const track = new Track({name, locations, userId: req.user._id});
+    await track.save();
+
+    res.send(track);}
+  catch(err){
+    res.status(422).send({error: err.message});
+  }
+
+})
+
+module.exports = router;

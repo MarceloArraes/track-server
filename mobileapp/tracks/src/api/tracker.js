fbd7fcace8de27281aa67adc9b3d13845de5a40f
MarceloArraes <marcelo.arraes@gmail.com>
2022-06-17 10:54:08 +0000

    fixed: Now working with cycled

--- mobileapp/tracks/src/api/tracker.js
+++ mobileapp/tracks/src/api/tracker.js
@@ -3,5 +3,5 @@ import axios from "axios";
 
 export default axios.create({
   //baseURL: "http://192.168.15.33:3000/tracks"
-  baseURL: "https://puce-lively-elk.cyclic.app/tracks"
+  baseURL: "https://puce-lively-elk.cyclic.app"
 })

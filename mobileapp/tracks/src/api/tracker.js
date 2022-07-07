ae4dcb304833f6d1dd847d6dfe78531ed887193f
MarceloArraes <marcelo.arraes@gmail.com>
2022-07-07 11:33:46 +0000

    improvement

--- mobileapp/tracks/src/api/tracker.js
+++ mobileapp/tracks/src/api/tracker.js
@@ -3,21 +3,19 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const instance = axios.create({
   //baseURL: "http://192.168.15.33:3000/tracks"
-  baseURL: "https://3c4c-2804-7f7-e288-19cd-a00-27ff-febb-f581.sa.ngrok.io/"
+  baseURL: "https://puce-lively-elk.cyclic.app/"
  })
 
 instance.interceptors.request.use(
   async (config) => {
-    const token = await AsyncStorage.getItem('token')
-    console.log("TOKEN ON INTERCEPTOR", token);
+    const token = await AsyncStorage.getItem("token");
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`
+      config.headers.Authorization = `Bearer ${token}`;
     }
-    return config
+    return config;
   },
-  (error) => {
-    console.log("error", error);
-    return Promise.reject(error);
+  (err) => {
+    return Promise.reject(err);
   }
 );
 
--- mobileapp/tracks/src/context/TrackContext.js
+++ mobileapp/tracks/src/context/TrackContext.js
@@ -19,15 +19,15 @@ const fetchTracks = async (dispatch) => {
 
 const createTrack = dispatch => async(name, locations) => {
   //console.log("createTrack", name, locations.length);
-  try{
-    await trackerApi.post("/tracks", { name, locations });
+  try {
+    await trackerApi.post('/tracks', { name, locations });
   }
   catch (err) {
     console.log(err);
+    console.log(err.message);
   }
+
 };
-
-
 
 export const { Provider, Context } = createDataContext(
   trackReducer,
